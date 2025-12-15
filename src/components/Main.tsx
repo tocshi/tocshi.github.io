e7d781fc11e4fca5096293dda75739e8a13f0f7d
Adam Wan <adam.x.wan@gmail.com>
2025-12-16 00:15:53 +0700

    Update projects

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import ProfilePicture from '../assets/images/profile.jpg';
 import '../assets/styles/Main.scss';
 
 function Main() {
@@ -9,7 +10,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="../assets/images/profile.png" alt="Avatar" />
+          <img src={ProfilePicture} alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import mock01 from '../assets/images/skirmish_mikofox.png';
-import mock02 from '../assets/images/project_lila.png';
-import mock03 from '../assets/images/chimerarms.png';
-import mock04 from '../assets/images/330proj_1.png';
-import mock05 from '../assets/images/304proj_1.png';
-import mock06 from '../assets/images/Castle4.png';
-import mock07 from '../assets/images/dungeonbg.png';
-import mock08 from '../assets/images/lifeofsaca.png';
+import cpsc330proj from '../assets/images/330proj_1.png';
+import cpsc304proj from '../assets/images/304proj_1.png';
+import lifeofsaca from '../assets/images/lifeofsaca.png';
+import castle0 from '../assets/images/Castle0.png';
+import castle1 from '../assets/images/Castle1.png';
+import castle2 from '../assets/images/Castle2.png';
+import castle3 from '../assets/images/Castle3.png';
 import '../assets/styles/Project.scss';
 
 function Project() {
@@ -15,44 +14,49 @@ function Project() {
         <h1>Projects</h1>
         <div className="projects-grid">
             <div className="project">
-                <a href="https://tocshi.itch.io/life-of-saca" target="_blank" rel="noreferrer"><img src={mock08} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://tocshi.itch.io/life-of-saca" target="_blank" rel="noreferrer"><h2>Astro Raiders</h2></a>
-                <p>Developed and released a 2D shooting game with C# and Unity. This project is hosted on the Itch.io public marketplace.</p>
+                <a href="https://tocshi.itch.io/life-of-saca" target="_blank" rel="noreferrer"><img src={lifeofsaca} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://tocshi.itch.io/life-of-saca" target="_blank" rel="noreferrer"><h2>Life of Saca</h2></a>
+                <p>A week-long game development project I made while learning Unity. After completing a few tutorials to learn some Unity and C# basics, I decided to challenge myself to complete a game project in a week. Inspired by <a href="https://twitter.com/epinesis/status/1666662584127467521" target="_blank" rel="noreferrer">this sensational tweet</a>, I set out to develop a small browser-playable game that's easy to pick up and offers just a few minutes of play time. <a href="https://tocshi.itch.io/life-of-saca" target="_blank" rel="noreferrer">Click here to try it out in your browser!</a></p>
             </div>
             <div className="project">
-                <a href="https://tocshi.itch.io/adrift-in-somnium" target="_blank" rel="noreferrer"><img src={mock07} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://tocshi.itch.io/adrift-in-somnium" target="_blank" rel="noreferrer"><h2>Datum: Integrated Learning Platform</h2></a>
-                <p>This is an online educational platform that provides high-quality, data science-focused learning resources in the Japanese language. I created the entire platform from scratch using Ruby on Rails.</p>
+                <iframe width="100%" className="zoom" style={{ aspectRatio: "16/9" }} src="https://www.youtube.com/embed/_nQ6M01RTg8" title="Adrift in Somnium - Trailer" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"/>
+                <a href="https://tocshi.itch.io/adrift-in-somnium" target="_blank" rel="noreferrer"><h2>Adrift in Somnium</h2></a>
+                <p>A project made for UBC's CPSC427 course. I led a team of 6 to create and finish a game project using C++ from scratch under tight deadlines and restrictions. I was in charge of implementing many features, such as combat, game progression, enemy behaviour, etc. along with organizing team planning and helping others with their feature implementations. I myself have also spent a substantial amount of time play-testing and improving the game based on feedback I've received from various testers. <a href="https://tocshi.itch.io/adrift-in-somnium" target="_blank" rel="noreferrer">Click here to download the game for yourself!</a></p>
             </div>
             <div className="project">
-                <img src={mock06} className="zoom" alt="thumbnail" width="100%"/>
-                <h2>WeManage: Real Estate Asset Management</h2>
-                <p>This mobile application allows realtors in Japan to securely manage their property information and view future income predictions. This app is built with Ruby on Rails and JavaScript.</p>
+                <img src={cpsc330proj} className="zoom" alt="thumbnail" width="100%"/>
+                <h2>Dataset Analysis Using Scikit Learn</h2>
+                <p>Throughout my Machine Learning course I worked with Python's <a href="https://scikit-learn.org/stable/" target="_blank" rel="noreferrer">Scikit Learn</a> and <a href="https://pandas.pydata.org/" target="_blank" rel="noreferrer">Pandas</a> libraries in Jupyter Lab to analyze datasets and train machine learning models to make predictions about new data. Shown is a screenshot of my analysis on a dataset of <a href="https://www.airbnb.com/" target="_blank" rel="noreferrer">Airbnb</a> rental listings, where I use <a href="https://shap.readthedocs.io/en/latest/" target="_blank" rel="noreferrer">SHAP</a> to determine which attributes of an Airbnb listing contribute the most to its popularity, which is measured by how many reviews it gets per month.</p>
             </div>
             <div className="project">
-                <img src={mock05} className="zoom" alt="thumbnail" width="100%"/>
-                <h2>COVID-19 Case Management</h2>
-                <p>Built official charts for COVID/vaccination tracking for an educational institution using JavaScript and the Google Sheets API v4. The dashboard served the university's leadership in their decision-making processes.</p>
+                <iframe width="100%" className="zoom" style={{ aspectRatio: "16/9" }} src="https://www.youtube.com/embed/4yDRdI9i6IA" title="Chimerarms - Demo" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"/>
+                <a href="https://tocshi.itch.io/chimerarms" target="_blank" rel="noreferrer"><h2>Chimerarms</h2></a>
+                <p>My submission for the game jam <a href="https://itch.io/jam/miz-jam-1" target="_blank" rel="noreferrer">Miz Jam 1</a>, where entrants have 48 hours to create a game using the <a href="https://kenney.nl/assets/bit-pack" target="_blank" rel="noreferrer">Kenney 1-Bit Pack</a> art pack for the game's graphics. I saw the art pack's various sprites for fantasy-style weapons and came up with an idea of mashing them together to create combinations of weapons that share some attributes from the weapons used to make them. It was a little challenging to scale the difficulty of the game with the random natures of the enemy spawns, but I was content with what I had submitted at the time of the deadline. <a href="https://tocshi.itch.io/chimerarms" target="_blank" rel="noreferrer">Click here to download the game for yourself!</a></p>
             </div>
             <div className="project">
-                <img src={mock04} className="zoom" alt="thumbnail" width="100%"/>
-                <h2>Multiple Regression Property Analysis</h2>
-                <p>Analyzed the real estate market in Japan and predicted property prices by implementing statistical methods such as OLS and multi-regression analysis. This project leveraged Python and various libraries such as Pandas, NumPy, Matplotlib, and Scikit-Learn.</p>
+                <div className="slideshow zoom">
+                    <img src={castle0} alt="Unreal Engine Castle Scene Slideshow" />
+                    <img src={castle1} alt="Unreal Engine Castle Scene Slideshow" />
+                    <img src={castle2} alt="Unreal Engine Castle Scene Slideshow" />
+                    <img src={castle3} alt="Unreal Engine Castle Scene Slideshow" />
+                </div>
+                <h2 style={{ paddingTop: "10px" }}>Unreal Engine Castle Scene</h2>
+                <p>A project I made to learn Unreal Engine 5. I followed various tutorials to learn UE5's interface and workflow, and familiarized myself with Nanite and Lumen, UE5's new mesh-rendering and lighting technologies. I also learned basic blueprinting to modify various aspects of the project. After learning the basics of using Unreal Engine 5 and its wide array of tools, I used various assets from Megascans and Quixel Bridge to construct a beautiful landscape and build some castles.</p>
             </div>
             <div className="project">
-                <a href="https://tocshi.itch.io/chimerarms" target="_blank" rel="noreferrer"><img src={mock03} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://tocshi.itch.io/chimerarms" target="_blank" rel="noreferrer"><h2>Programs of Study</h2></a>
-                <p>Designed and developed a custom component for a CMS-based platform (e.g., 'Brightspot') using Java, Handlebars, and LESS. University students can find their majors of interest through this module.</p>
+                <iframe width="100%" className="zoom" style={{ aspectRatio: "16/9" }} src="https://www.youtube.com/embed/JDJRBdMOogY" title="Project Lila - Demo" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"/>
+                <a href="https://youtu.be/JDJRBdMOogY" target="_blank" rel="noreferrer"><h2>Project Lila Concept</h2></a>
+                <p>Based on an Fantasy Action RPG idea I've had for several years, I worked with <a href="https://github.com/c0linw" target="_blank" rel="noreferrer">c0linw</a> and <a href="https://github.com/MichaelXian" target="_blank" rel="noreferrer">MichaelXian</a> to develop a game based on this idea. We used this project to learn about the GameMaker engine and experiment with implementing a proof-of-concept ARPG combat system, using GameMaker Studio 2. However, due to engine limitations, the project has been put on hold, and will resume development in a different form in the future. If you are interested in seeing the entire development update playlist, you can find it <a href="https://www.youtube.com/playlist?list=PLwmx6LhN4gN7uc7KSynXkGjq_d9XXSTIA" target="_blank" rel="noreferrer">here</a>.</p>
             </div>
             <div className="project">
-                <a href="https://youtu.be/JDJRBdMOogY" target="_blank" rel="noreferrer"><img src={mock02} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://youtu.be/JDJRBdMOogY" target="_blank" rel="noreferrer"><h2>Transfer Evaluation Matrix</h2></a>
-                <p>Created an interactive CSV table generator with Java, Handlebars, and LESS. This project helps transfer students to quickly identify eligible credits.</p>
+                <iframe width="100%" className="zoom" style={{ aspectRatio: "16/9" }} src="https://www.youtube.com/embed/t1HCjLRCn9I" title="Skirmish with the Miko Fox - Demo" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"/>
+                <a href="https://youtu.be/t1HCjLRCn9I" target="_blank" rel="noreferrer"><h2>Skirmish with the Miko Fox</h2></a>
+                <p>A <a href="https://en.wikipedia.org/wiki/Touhou_Project" target="_blank" rel="noreferrer">Touhou Project</a>-styled game written using the C-like language <a href="https://dmf.shrinemaiden.org/wiki/Main_Page" target="_blank" rel="noreferrer">Danmakufu ph3</a>. I intended for the game to consist of a single "boss fight" encounter that focused on displaying colourful projectile patterns rather than conventional gameplay. I started making individual scripts using Danmakufu in August of 2018, and started working on a more unified game in January of 2019.</p>
             </div>
             <div className="project">
-                <a href="https://youtu.be/t1HCjLRCn9I" target="_blank" rel="noreferrer"><img src={mock01} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://youtu.be/t1HCjLRCn9I" target="_blank" rel="noreferrer"><h2>Submeowrine</h2></a>
-                <p>Developed and released an Android mobile application using Java and Android Studio that runs a 2D shooting game.</p>
+                <img src={cpsc304proj} className="zoom" alt="thumbnail" width="100%"/>
+                <h2>Data Manager Mini Application</h2>
+                <p>A project for my Databases course where I worked with a small team to make a Java application that uses SQL to view and manage the values of pre-made data tables. A simple GUI is implemented to make our application user-friendly and human-readable.</p>
             </div>
         </div>
     </div>
